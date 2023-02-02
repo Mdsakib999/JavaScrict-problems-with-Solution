72238642ad7c93ae5b391818190b7e5093ce1eea
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-02 23:49:21 +0600

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,20 @@
+// Find all even numbers from 1 - 10?
+
+var num = 0;
+
+while(num <= 10) {
+    console.log(num);
+    num += 2;
+}
+
+
+
+
+// Find all odd numbers from 1 - 10?
+
+var num = 1;
+
+while(num <= 10) {
+    console.log(num);
+    num += 2;
+}
