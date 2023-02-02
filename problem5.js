64cbd6f91d09270a45b14464a100d3d362bc6abd
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-03 00:00:11 +0600

    Add files via upload

---
+++ problem5.js
@@ -0,0 +1,21 @@
+// Print a table containing multiplication tables?
+
+
+
+var i, sum, a = 5;
+
+for(i = 1; i <= 10; i++){
+    sum = a * i;
+    console.log(a,"X", i,"=", sum);
+}
+
+
+/* function mult_tabl(num) {
+    let sum = 0;
+    for(var i = 0; i <= num; i++) {
+        sum = num * i;
+        console.log(num,"X", i,"=", sum);
+    }
+}
+
+mult_tabl(6) */
