b0c7bb7d365fc3fee77f24ff396c05e1c1a253d7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-11 23:39:13 +0600

    Add files via upload

---
+++ problem6.js
@@ -0,0 +1,15 @@
+// Simple function related Problem. Calculate your average marks?
+
+
+function ave(a, b, c) {
+    var total_ave = (a + b + c) / 3;
+    return total_ave; 
+}
+
+var assignment1 = 50;
+var assignment2 = 80;
+var assignment3 = 60;
+
+var myAverage= ave(assignment1, assignment2, assignment3);
+
+console.log("My average marks:", myAverage.toFixed(2))
