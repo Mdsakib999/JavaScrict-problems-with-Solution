c20e78ecd434499a3bacf6cc1d5b725c073948e7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-15 23:38:37 +0600

    Add files via upload

---
+++ problem10.js
@@ -0,0 +1,40 @@
+// Write a program to show output of multiplication table?
+
+
+
+
+var i;
+
+function multi_table(a) {
+    for(var i = 1; i <= 10; i++){
+        var sum = a * i;
+
+        return i;
+        // console.log(a,"X", i,"=", sum);
+    }
+    return sum;
+}
+
+
+var num = 6;
+var m = multi_table(num);
+console.log(num,"X", i,"=", m);
+
+
+/* function multi_table(a) {
+    for(var i = 1; i <= 10; i++){
+        var sum = a * i;
+        console.log(a,"X", i,"=", sum);
+    }
+}
+
+multi_table(6) */
+
+
+
+/* var i, sum, a = 9;
+
+for(i = 1; i <= 10; i++){
+    sum = a * i;
+    console.log(a,"X", i,"=", sum);
+} */
