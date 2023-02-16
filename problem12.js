295d788885305887f79059a317be8db706536eb1
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-16 23:53:03 +0600

    Add files via upload

---
+++ problem12.js
@@ -0,0 +1,16 @@
+// Factorial
+
+
+function factorial(num) {
+   let sum = 1;
+    for(let i = 1; i<= num; i++) {
+        sum = sum * i
+    }
+
+    return sum;
+}
+
+
+let number = 5;
+let my_fact = factorial(number);
+console.log("Factorial of",number,"is", my_fact);
