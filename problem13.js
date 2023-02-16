2928360581640d7bb8ede00700688d7b1c656ba1
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-16 23:55:04 +0600

    Add files via upload

---
+++ problem13.js
@@ -0,0 +1,17 @@
+// Write a function findSum() that will take the array [5, 7, 8, 10, 45, 30] as the input parameter and will return the sum of all array element.
+
+
+function findSum(num) {
+
+    let sum = 0;
+    for(let i= 0; i < num.length; i++) {
+        let element = num[i];
+        sum += element;
+    }
+    return sum;
+}
+
+
+let arr = [5, 7, 8, 10, 45, 30];
+
+console.log("Total sum of all element =",findSum(arr));
