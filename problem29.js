fe12d0be68dd155897dc7b12894ac98e7cc00945
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-03-13 23:47:57 +0600

    commit

---
+++ problem29.js
@@ -0,0 +1,11 @@
+// Check element have in array or not?
+
+
+var str = "1010101"
+
+for( var i = 0; i < str.length; i++) {
+    // console.log(str[i]);
+    if(str[i] == 0) {
+        console.log('zero')
+    }
+}
