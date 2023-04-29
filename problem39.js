5e1a4b235782ae965e835804401500e6820edbcc
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-04-29 23:27:55 +0600

    commit

---
+++ problem39.js
@@ -0,0 +1,15 @@
+// find positive element from an array
+
+
+var arr = [10, 20, 30, -40, 22,14];
+var ps_arr = [];
+
+for(var i =0; i< arr.length; i++) {
+  if(arr[i] > 0) {
+    ps_arr.push(arr[i]);
+  }
+  else{
+    break;
+  }
+}
+console.log(ps_arr);
