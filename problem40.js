538a08d592a872d9abb4759a978122a28ab76b02
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-04-29 23:28:26 +0600

    commit

---
+++ problem40.js
@@ -0,0 +1,15 @@
+// for of and for in loop
+
+
+let array = [5,66,4,88,90];
+
+    // shows array element.
+for(let i of array) {
+    console.log(i);
+}
+
+
+    // shows array index.
+for(var i in array) {
+    console.log(i);
+}
