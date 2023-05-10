44e53d894d969e825f4d0a6c70b25b5df992edb2
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-05-10 23:26:05 +0600

    commit

--- problem45.js
+++ problem45.js
@@ -1,3 +1,18 @@
 
-const a = new Date("11/1/2022");
-console.log(a)
+
+
+
+
+
+let curday = function(sp){
+today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1; //As January is 0.
+let yyyy = today.getFullYear();
+
+if(dd<10) dd='0'+dd;
+if(mm<10) mm='0'+mm;
+return (mm+sp+dd+sp+yyyy);
+};
+console.log(curday('/'));
+console.log(curday('-'));
