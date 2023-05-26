0c9b9d8d40bb767c5bb45930429d9b3e58d49bdd
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-05-26 14:37:53 +0600

    commit

---
+++ sum.js
@@ -0,0 +1,51 @@
+/* /* /* This function takes 3 number as parameter and calculate them according to given instructions. If the total sum of the calculations is less than 2000, return the sum as output. If the total sum is greater than 2000 then subtract 2000 from the total value and return the result as output. */
+
+/* 
+function gemsToDiamond(firstFriendGem, secondFriendGem, thirdFriendGem) {
+
+    if(typeof firstFriendGem == "number" && typeof secondFriendGem == "number" && typeof thirdFriendGem == "number") {
+
+        let firstFriendDiamond = firstFriendGem *21;
+
+        let secondFriendDiamond = secondFriendGem * 32;
+        let thirdFriendDiamond = thirdFriendGem * 43;
+    
+        let totalDiamond = firstFriendDiamond + secondFriendDiamond + thirdFriendDiamond;
+    
+        if( totalDiamond < 2000) {
+            return totalDiamond;
+        }
+    
+        else{
+            let restDiamond = totalDiamond - 2000;
+    
+            return restDiamond;
+        }
+    }
+
+    else{
+        return "provide all parameter as number"
+    }
+}
+
+console.log(gemsToDiamond(5,6,2))  */
+
+
+
+function isLGSeven(number) {
+    if(typeof number == "number") {
+    let result = number - 7;
+    if(result < 7) {
+    return result;
+    }
+    else{
+    return number * 2;
+    }
+    }
+    else {
+    return "Enter a number";
+    }
+    }
+
+
+    console.log(isLGSeven(90))
