f0156240c72925e5108b39ab6f07153e4a47142d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-13 23:07:12 +0600

    commit

--- problem60.js
+++ problem60.js
@@ -3,3 +3,15 @@
 
 
 
+function test_same_digit(num) {
+  var first = num % 10;
+  while (num) {
+    if (num % 10 !== first) return false;
+num = Math.floor(num / 10);
+  }
+  return true
+}
+
+console.log(test_same_digit(2146854));
+console.log(test_same_digit(5555));
+console.log(test_same_digit(9889));
