639134ea48aebf833854cb7fed3ed711f43d2ea9
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-20 00:50:44 +0600

    commit

--- problem65.js
+++ problem65.js
@@ -15,3 +15,20 @@ console.log(result);
 
 ------------------------------------------------
   
+// Another Way
+
+  function removeDuplicates(num) {
+  var out = [];
+  for (var i = 0; i < num.length; i++) {
+    if (out.indexOf(num[i]) === -1) {
+      out.push(num[i]);
+    }
+  }
+  return out;
+}
+
+var Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+var result = removeDuplicates(Mynum);
+
+console.log(Mynum);
+console.log(result);
