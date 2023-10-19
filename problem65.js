04b2b4b4d4845768a837525f234845fe7bfe1791
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-20 00:49:56 +0600

    Update problem65.js

--- problem65.js
+++ problem65.js
@@ -1,1 +1,17 @@
 // Remove duplicate items from an array using JavaScript?
+
+
+// 1 way
+
+function removeDuplicates(num) {
+  return Array.from(new Set(num));
+}
+
+var Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+var result = removeDuplicates(Mynum);
+
+console.log(Mynum);
+console.log(result);
+
+------------------------------------------------
+  
