99a06fb972e89a81fa3ba233b0750cb562b63412
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-27 23:58:53 +0600

    commit

--- problem66.js
+++ problem66.js
@@ -1,1 +1,14 @@
 // JavaScript function that accepts an argument and check the type returns.??
+
+
+function detectDataType(value) {
+  const types = [Function, RegExp, Number, String, Boolean, Object];
+  for (const type of types) {
+    if (value instanceof type) return type;
+  }
+  return typeof value;
+}
+
+console.log(detectDataType(12));
+console.log(detectDataType('w3resource'));
+console.log(detectDataType(false));
