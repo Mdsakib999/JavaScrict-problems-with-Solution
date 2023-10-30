2d957cfa393457c11256a824f2ba8d33e352151d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-31 00:03:40 +0600

    commit

--- problem67.js
+++ problem67.js
@@ -1,1 +1,10 @@
-// Write a JavaScript program that creates a button and add a click event listener to log a message when it's clicked.
+// Q:  Create a button and show a message by clicking on it using event listener in Javascript.
+
+
+
+const button = document.createElement('button');
+button.textContent = 'Click me';
+
+button.addEventListener('click', () => console.log('Button clicked!'));
+
+document.body.appendChild(button);
