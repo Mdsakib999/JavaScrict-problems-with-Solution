c0f89e2e401db39eba6ee008ce2565728bb4f553
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-31 00:16:51 +0600

    commit

--- problem68.js
+++ problem68.js
@@ -15,7 +15,20 @@
 <body>
   
 <div id="myDiv" class="my-element"></div>
-  
+
+
+   <script>
+    function changeBackgroundColor(elementId, color) {
+      const element = document.getElementById(elementId);
+      if (element) {
+        element.addEventListener('mouseenter', () => {
+          element.style.backgroundColor = color;
+        });
+      }
+    }
+
+   
+  </script>
 </body>
 </html>
 
