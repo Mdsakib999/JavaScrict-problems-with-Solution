7282f55354d15f3b15b509cb4e0df4af91b1c453
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-11-03 00:04:47 +0600

    commit

--- problem69.js
+++ problem69.js
@@ -1,1 +1,11 @@
 // JavaScript exercise to get the filename extension.
+
+
+// **************HTML code****************
+
+
+filename = "system.php"
+console.log(filename.split('.').pop());
+
+filename = "abc.js"
+console.log(filename.split('.').pop());
