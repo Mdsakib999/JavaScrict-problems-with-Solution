ad94ef473a6ebbfaedd2f646ec974694bd40176f
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-11-08 23:56:46 +0600

    Update problem71.js

--- problem71.js
+++ problem71.js
@@ -1,1 +1,17 @@
 // 
+
+
+
+//#Source https://bit.ly/2neWfJ2
+function pull(arr, ...args) {
+  let pulled = [];
+  arr.forEach((v, i) => {
+    if (!args.includes(v)) {
+      pulled.push(v);
+    }
+  });
+  arr.length = 0;
+  pulled.forEach(v => arr.push(v));
+  return pulled;
+}
+
