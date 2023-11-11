5e89d6079f09aea250768d9462490d86292e2ca3
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-11-11 22:31:09 +0600

    commit

--- problem72.js
+++ problem72.js
@@ -1,1 +1,9 @@
 // Write a program to convert a given string's length to bytes.
+
+
+
+const byte_Size = str => new Blob([str]).size;
+console.log(byte_Size('Ã¢')); 
+console.log(byte_Size('Hello World'));  
+console.log(byte_Size('123456'));  
+
