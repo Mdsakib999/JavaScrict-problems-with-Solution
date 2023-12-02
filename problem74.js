23ba087f58945014760a430efc7065ac0e3dd7f8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-12-02 21:18:14 +0600

    commit

--- problem74.js
+++ problem74.js
@@ -3,3 +3,15 @@
 
 
 
+// Create a function called capitalizeFirstLetter that takes a string as an argument
+function capitalizeFirstLetter(str) {
+    
+    let wordsArray = str.split(" ");
+    wordsArray = wordsArray.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    let resultString = wordsArray.join(" ");
+
+    return resultString;
+}
+
+console.log(capitalizeFirstLetter("Write a JavaScript program to capitalize the first letter of each word of a given string."));
