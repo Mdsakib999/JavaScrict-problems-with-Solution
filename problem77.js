e19082a794cd17a5f263ec1b4f41ff5a1a76968e
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-12-30 23:20:42 +0600

    Update problem77.js

--- problem77.js
+++ problem77.js
@@ -1,1 +1,5 @@
 // Code a program to find the number of elements in both arrays using Javascript.
+
+@param {Array} arr1 - The first array
+@param {Array} arr2 - The second array
+@returns {number} - The count of common elements
