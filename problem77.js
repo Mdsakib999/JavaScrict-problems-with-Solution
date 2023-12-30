2702631736cab39143e93b62ad94e8c8c36c54ce
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-12-30 23:21:19 +0600

    Update problem77.js

--- problem77.js
+++ problem77.js
@@ -3,3 +3,7 @@
 @param {Array} arr1 - The first array
 @param {Array} arr2 - The second array
 @returns {number} - The count of common elements
+
+function countCommonElements(arr1, arr2) {
+  return arr1.filter(element => arr2.includes(element)).length;
+};
