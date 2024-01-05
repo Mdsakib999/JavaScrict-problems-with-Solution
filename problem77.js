6d6092b03596f8da0fd5058a512570e8596d0874
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-01-05 23:26:07 +0600

    commit

--- problem77.js
+++ problem77.js
@@ -8,6 +8,6 @@ function countCommonElements(arr1, arr2) {
   return arr1.filter(element => arr2.includes(element)).length;
 };
 
-console.log(countCommonElements([1, 2, 3, 4], [1, 2, 3, 4])); // Output: 4
-console.log(countCommonElements([1, 2, 3, 4], [1, 2, 3, 5])); // Output: 3
-console.log(countCommonElements([1, 2, 3, 4], [11, 22, 33, 44]));
+console.log(countCommonElements([1, 2, 3, 4], [1, 2, 3, 4]));                 // Output: 4
+console.log(countCommonElements([1, 2, 3, 4], [1, 2, 3, 5]));                 // Output: 3
+console.log(countCommonElements([1, 2, 3, 4], [11, 22, 33, 44]));             //
