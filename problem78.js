c6deb94b11158e403159061645d2532c56685f26
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-01-15 12:44:10 +0600

    commit

--- problem78.js
+++ problem78.js
@@ -1,1 +1,25 @@
 // Q: Write a function to check the given objects are equal or not, and how does it determine the equality of two objects?
+
+
+function ObjectsEqual(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (let key of keys1) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  const object1 = { a: 1, b: 2 };
+  const object2 = { b: 2, a: 1 };
+  
+  console.log(ObjectsEqual(object1, object2));
+  
