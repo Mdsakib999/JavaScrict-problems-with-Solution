4110d313d57c8993ed5e622fdf4a6280610aee35
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-01-28 17:26:38 +0600

    Update problem79.js

--- problem79.js
+++ problem79.js
@@ -1,1 +1,9 @@
 // JavaScript program to print (Print Page) the current window contents.
+
+
+// HTML code
+<body>
+<p></p>
+<p>Click the button to print the current page.</p>
+<button onclick="print_current_page()">Print this page</button>
+</body>
