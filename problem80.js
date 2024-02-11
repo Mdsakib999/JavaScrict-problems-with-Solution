b5e4844b4fb9c9d4100fd3d3af7fb29f254a2621
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-02-11 23:58:40 +0600

    Update problem80.js

--- problem80.js
+++ problem80.js
@@ -1,1 +1,11 @@
 
+
+
+function all_max(nums) {
+    return Array(3).fill(Math.max(nums[0], nums[2]));
+}
+
+console.log(all_max([20, 30, 40]));   // Output: [40, 40, 40]
+console.log(all_max([-7, -9, 0]));    // Output: [0, 0, 0]
+console.log(all_max([12, 10, 3]));    // Output: [12, 12, 12]
+
