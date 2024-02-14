9cf9555f2e58f5d7fd9c75b65ef467ee33f41958
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-02-15 00:10:14 +0600

    commit

--- problem73.js
+++ problem73.js
@@ -1,1 +1,7 @@
 // Write a program to get the nth element of a given array of elements using JavaScript.
+
+
+const nthElement = (arr, n = 0) => (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+console.log(nthElement(['a', 'b', 'c'], 1));
+console.log(nthElement(['a', 'b', 'b'], -3));
+
