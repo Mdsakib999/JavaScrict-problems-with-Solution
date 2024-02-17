50b97908f0f2a865c562985a8d6832668e8f2a1d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-02-17 23:28:15 +0600

    Update problem75.js

--- problem75.js
+++ problem75.js
@@ -1,1 +1,12 @@
 // Write a function that accepts two numbers as parameters and Return a custom 'Error' if the second number is zero using JavaScript.
+
+
+const divide_Numbers = (n1, n2) => {
+  if (n2 === 0) {
+    throw new Error('Error: Division by zero is not allowed.');
+  }  
+  return n1 / n2;
+};
+
+
+
