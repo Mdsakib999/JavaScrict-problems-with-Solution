28f87e3266fc930f2155468ef4f4f39acc1438eb
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-03-01 23:56:24 +0600

    Update problem81.js

--- problem81.js
+++ problem81.js
@@ -1,1 +1,10 @@
 // Write a JavaScript function that returns a string that has letters in alphabetical order using JS?
+
+
+// Define a function named alphabet_order that takes a string parameter (str)
+function alphabet_order(str) {
+  // Split the string into an array of characters, sort the array, and join the characters back into a string
+  return str.split('').sort().join('');
+}
+
+
