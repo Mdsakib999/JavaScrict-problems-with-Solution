f4175d22c4b16250d30abc89e07f14cd15392ca2
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-03-09 23:16:43 +0600

    Update problem82.js

--- problem82.js
+++ problem82.js
@@ -1,3 +1,10 @@
 // Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers?
 
 
+function Second_Greatest_Lowest(arr_num) {
+  arr_num.sort((x, y) => x - y); 
+  let unique = [...new Set(arr_num)]; 
+  return [unique[1], unique[unique.length - 2]].join(','); // Return second smallest and second largest
+}
+
+
