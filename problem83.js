112f6be105d291b898092729a1266d92c4ee98d7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-03-31 23:32:50 +0600

    Update problem83.js

--- problem83.js
+++ problem83.js
@@ -1,1 +1,11 @@
 // 
+
+
+const testSameDigit = num => {
+  const digit = num % 10;
+  return num.toString().split('').every(d => d === digit.toString());
+};
+
+console.log(testSameDigit(1234));
+console.log(testSameDigit(1111));
+console.log(testSameDigit(22222222));
