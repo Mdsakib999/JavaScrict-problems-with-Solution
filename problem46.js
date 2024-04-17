e07cff8181e2bde1dba07388b705a250c2a99e60
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-04-17 12:40:26 +0600

    commit

--- problem46.js
+++ problem46.js
@@ -1,7 +1,7 @@
-//Q: Write a JavaScript function to add specified minutes to a Date object.?
+//Q: Write a JavaScript function to add specified minutes to a Date object?
 
 
 let add_minutes =  function (dt, minutes) {
-    return new Date(dt.getTime() + minutes*60000);
+    return new Date(dt.getTime() + minutes * 60000);
 }
 console.log(add_minutes(new Date(2014,10,2), 30).toString());
