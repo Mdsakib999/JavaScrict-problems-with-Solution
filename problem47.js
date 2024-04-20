a5e9165ac3dffb165eaeda4c89febdd5b9415cb9
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-04-20 11:13:01 +0600

    commit

--- problem47.js
+++ problem47.js
@@ -4,4 +4,6 @@
 
 const last_digit = (x, y, z) => x > 0 && y > 0 && z > 0 && x % 10 === y % 10 && y % 10 === z % 10 && x % 10 === z % 10;
 
-
+console.log(last_digit(20, 30, 400));
+console.log(last_digit(-20, 30, 400));
+console.log(last_digit(20, 30, -400));
