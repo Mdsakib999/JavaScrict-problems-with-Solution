1576b5a1c16298694db02d8e8103a62b392ddde9
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-04-25 22:04:37 +0600

    Update problem49.js

--- problem49.js
+++ problem49.js
@@ -1,1 +1,13 @@
 // Q: Sort the itemd of an array using JS.
+
+
+
+// Original array
+var arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+
+// Sorted array
+var arr2 = [];
+
+var max = Math.max(...arr1);
+
+
