03301ddfc30703e18b94c6862479b022253b47d8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-10-13 07:55:07 -0700

    commit

--- problem85.js
+++ problem85.js
@@ -1,2 +1,10 @@
 // Check if a String is a Palindrome or not using JS.
 
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+// Example usage:
+console.log(isPalindrome("racecar")); // Output: true
+console.log(isPalindrome("hello"));   // Output: false
