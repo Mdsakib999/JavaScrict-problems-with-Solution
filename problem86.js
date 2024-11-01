8bc23fc5892e06f86c72584a15b70020d33df59a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-11-01 07:49:58 -0700

    commit

--- problem86.js
+++ problem86.js
@@ -1,1 +1,16 @@
-// 
+// Loading Component
+
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex space-x-2">
+                <div className="h-8 w-8 bg-green-500 rounded-full animate-bounce"></div>
+                <div className="h-8 w-8 bg-green-500 rounded-full animate-bounce200"></div>
+                <div className="h-8 w-8 bg-green-500 rounded-full animate-bounce400"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
+
