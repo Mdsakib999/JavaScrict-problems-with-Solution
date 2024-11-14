c2ea62d6d5500ff6c44d5eb719bd178779dda0bf
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2024-11-14 04:53:05 -0700

    Update problem88.js

--- problem88.js
+++ problem88.js
@@ -1,1 +1,18 @@
-// 
+// Group Anagrams
+
+
+function groupAnagrams(strs) {
+    const map = {};
+
+    for (let str of strs) {
+        const sortedStr = str.split('').sort().join('');
+        if (!map[sortedStr]) map[sortedStr] = [];
+        map[sortedStr].push(str);
+    }
+
+    return Object.values(map);
+}
+
+// Example usage:
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+// Output: [ [ 'eat', 'tea', 'ate' ], [ 'tan', 'nat' ], [ 'bat' ] ]
